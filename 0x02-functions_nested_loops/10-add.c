3fd5e0e6fe9aded13c409cfec126e75e9aca9396
DedeDoe <lomokie2022@gmail.com>
2023-08-17 07:40:54 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ *add -Entry point
+ *Description -Adds 2 integers
+  * @a: int to be added to b
+	 * @b: int to be added to a
+ *Return: Always 0 successful
+ **/
+
+	int add(int a, int b)
+	{
+		int c;
+
+
+		c = a + b;
+
+
+		return (c);
+	}
+
